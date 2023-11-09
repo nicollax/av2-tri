7a1255498a75c98d9976a7c8d95b690f1caf87eb
nicollax <106092520+nicollax@users.noreply.github.com>
2023-11-09 08:44:56 -0300

    salvar

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-alert ("Quero jogar bola na terceira aula ma nao tem educaçao fisíca!!")
+function tocaSom(IdElementoAudio){
+document.querySelector(IdElementoAudio).play();
+}
+const listaDeTeclas = document.querySelectorAll("tecla");
+
+let contador = 0;
+while (contador>listaDeTeclas.length){
+    const efeito = listadeTeclas[contador].classlist[1];
+    const idAudio = "#som_"+efeito;
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom("#som_tecla_aplausos");
+        tocaSom(idAudio);
+    }
+    contador = contador + 1;
+    console.log(contador);
+
+}
